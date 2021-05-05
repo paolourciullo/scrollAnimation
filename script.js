226df324e1cb20c53afeabdcf54faf955f7ccddb
Paolo U <paolourciullo@gmail.com>
2021-05-05 09:18:57 -0400

    add api cxn

--- script.js
+++ script.js
@@ -1,20 +1,107 @@
-const boxes = document.querySelectorAll('.box')
+const APIURL = 'https://api.github.com/users/';
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const boxes = document.querySelectorAll('.box');
 
-window.addEventListener('scroll', checkBoxes)
+window.addEventListener('scroll', checkBoxes);
 
-checkBoxes()
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+
+    createUserCard(data);
+    getRepos(username);
+  } catch (err) {
+    if (err.response.status == 404) {
+      createErrorCard('No profile with this username.');
+    }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created');
+
+    addReposToCard(data);
+  } catch (err) {
+    createErrorCard('Problem fetching repos.');
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = `<div class="card">
+    <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+
+    <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+            <li>${user.followers} <strong>followers</strong></li>
+            <li>${user.following} <strong>following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos">
+            
+        </div>
+    </div>
+</div>
+`;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+      <div class="card">
+        <h1>${msg}</h1>
+      </div>
+    `;
+  main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos');
+
+  repos.slice(0, 10).forEach((repo) => {
+    const repoEl = document.createElement('a');
+    repoEl.classList.add('repo');
+    repoEl.href = repo.html_url;
+    repoEl.target = '_blank';
+    repoEl.innerText = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = '';
+  }
+});
+
+checkBoxes();
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
+  const triggerBottom = (window.innerHeight / 5) * 4;
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
 
-        if(boxTop < triggerBottom) {
-            box.classList.add('show')
-
-        } else {
-            box.classList.remove('show')
-        }
-    })
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+      console.log();
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
